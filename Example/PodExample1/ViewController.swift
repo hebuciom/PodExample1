d84630e958adaeba9490171e51c59c3dd08836e4
hebuciom <hebuciom@liverpool.com.mx>
2023-11-20 21:59:20 -0600

    Initial commit

---
+++ Example/PodExample1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PodExample1
+//
+//  Created by hebuciom on 11/20/2023.
+//  Copyright (c) 2023 hebuciom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
